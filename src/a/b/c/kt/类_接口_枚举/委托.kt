93356bd8f3a107e975df204088c128e4618d4e00
duxl <645575165@qq.com>
2023-05-24 14:09:39 +0800

    委托

---
+++ src/a/b/c/kt/类_接口_枚举/委托.kt
@@ -0,0 +1,22 @@
+package a.b.c.kt.类_接口_枚举
+
+interface Base {
+    fun show(str: String)
+}
+
+class BaseImpl : Base {
+    override fun show(str: String) {
+        println("base impl show: $str")
+    }
+}
+
+class Driver(b: Base) : Base by b {
+    // 如果Driver覆盖了此方法，那么不会调用委托实现的方法
+    /*override fun show(str: String) {
+        println("driver $str")
+    }*/
+}
+
+fun main() {
+    Driver(BaseImpl()).show("123")
+}
---
+++ src/a/b/c/kt/类_接口_枚举/委托属性.kt
@@ -0,0 +1,29 @@
+package a.b.c.kt.类_接口_枚举
+
+import kotlin.reflect.KProperty
+
+class Delegate {
+    // thisRef参数类型限制委托可作用与那个类型上
+    // property参数可取得委托作用与类型的哪个属性
+    operator fun getValue(thisRef: Example?, property: KProperty<*>): String {
+        return "$thisRef, thank you for delegating '${property.name}' to me!"
+    }
+
+    operator fun setValue(thisRef: Example?, property: KProperty<*>, value: String) {
+        println("$value has ben assigned to '${property.name}' in $thisRef.")
+    }
+}
+
+class Example {
+    var p: String by Delegate()
+}
+
+fun main() {
+    /*var str: String by Delegate()
+    str = "abc"
+    println(str)*/
+
+    var e = Example()
+    println(e.p)
+    e.p = "abc"
+}
