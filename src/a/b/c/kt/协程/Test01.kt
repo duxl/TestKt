1924375ec8d62d7481b040cafae03fce4567fb79
duxl <645575165@qq.com>
2023-05-24 17:21:42 +0800

    runBlocking、delay挂起函数、join阻塞等待

--- src/a/b/c/kt/协程/Test01.kt
+++ src/a/b/c/kt/协程/Test01.kt
@@ -1,7 +1,9 @@
 package a.b.c.kt.协程
 
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 /**
  * 普通工程中添加coroutines的依赖：
@@ -11,11 +13,43 @@ import kotlinx.coroutines.launch
  * https://www.kotlincn.net/docs/reference/coroutines/coroutines-guide.html
  */
 fun main() {
+    //test1()
+    //test2()
+    test3()
+}
+
+fun test1() {
     //  GlobalScope 启动协程的生命周期只受整个应用程序的生命周期限制
     GlobalScope.launch {
         printThread("协程中打印")
     }
     printThread("main中打印")
     // 这里需要延迟，不然主线程很有可能比协程提前结束
-    Thread.sleep(1000)
+    Thread.sleep(1000) // 阻塞main函数
+}
+
+fun test2() {
+    //  GlobalScope 启动协程的生命周期只受整个应用程序的生命周期限制
+    GlobalScope.launch {
+        printThread("协程中打印")
+    }
+    printThread("main中打印")
+    runBlocking { // runBlocking会阻塞所在代码块的线程
+        printThread("runBlocking中打印")
+        delay(1000) // 挂起main函数
+    }
+}
+
+fun test3() {
+    // 延迟一段时间来等待另一个协程运行并不是一个好的选择
+    // 使用join更加合理
+    runBlocking {
+        //  GlobalScope 启动协程的生命周期只受整个应用程序的生命周期限制
+        val job = GlobalScope.launch {
+            printThread("协程中打印")
+        }
+        printThread("main中打印")
+        job.join() // 等待协程结束
+        printThread("main中打印Over")
+    }
 }
